a003030d9b29110897348441249a3e2e28fc0f19
Julien Brunet <julien.brunet.92@gmail.com>
2020-05-26 17:24:49 +0200

    replaced google services

--- src/services/AppConfig.service.tsx
+++ src/services/AppConfig.service.tsx
@@ -52,16 +52,16 @@ export default class AppConfig {
     prod: {
       searchId: '',
       searchSecret: '',
-      stripeAPIKey: '',
+      stripeAPIKey: 'pk_test_ZqfQqyALkPrkNMasg3kwvFE5',
       firebaseOptions: {
-        apiKey: "AIzaSyBXkiqx8Q3OgS1HlGYMfknFcZj8y8topKU",
-        authDomain: "all-is-good-dev.firebaseapp.com",
-        databaseURL: "https://all-is-good-dev.firebaseio.com",
-        projectId: "all-is-good-dev",
-        storageBucket: "all-is-good-dev.appspot.com",
-        messagingSenderId: "467075255307",
-        appId: "1:467075255307:web:c31e6277e1d9dcfb460b0e",
-        measurementId: "G-P5QKKZXYZY"
+        apiKey: "AIzaSyB7zLRgccH8E9oNBmtME_lan7zAkI2P3ds",
+        authDomain: "all-is-good-prod.firebaseapp.com",
+        databaseURL: "https://all-is-good-prod.firebaseio.com",
+        projectId: "all-is-good-prod",
+        storageBucket: "all-is-good-prod.appspot.com",
+        messagingSenderId: "1039569656278",
+        appId: "1:1039569656278:web:f034aaef5395c99242ac77",
+        measurementId: "G-9WC0Q75J2R"
       }
     }
   }

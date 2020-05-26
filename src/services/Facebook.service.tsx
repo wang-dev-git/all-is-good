867b7717f4ef68a9662998a6fd2f50112bebef64
Julien Brunet <julien.brunet.92@gmail.com>
2020-05-26 13:24:25 +0200

    ok

--- src/services/Facebook.service.tsx
+++ src/services/Facebook.service.tsx
@@ -6,20 +6,18 @@ export default class FacebookService {
 
   // Login to facebook
   static async login() {
-    const {
-      type,
-      token,
-      expires,
-    } = await Facebook.logInWithReadPermissionsAsync(AppConfig.facebookId, {
+    await Facebook.initializeAsync(AppConfig.facebookId, "All is Good")
+    const loginRes = await Facebook.logInWithReadPermissionsAsync({
       permissions: ['public_profile', 'email'],
     });
-    if (type === 'success') {
+    if (loginRes.type === 'success') {
+      const token = loginRes.token
       // Get the user's name using Facebook's Graph API
-      const res = await fetch(`https://graph.facebook.com/me?fields=email,first_name,last_name,picture.width(500).height(500) &access_token=${token}`);
+      const res = await fetch(`https://graph.facebook.com/me?fields=email,first_name,last_name,picture.width(500).height(500)&access_token=${token}`);
       const user = await res.json()
-      return { user, token }
+      return { user, token: token }
     }
-    return { user: null, token}
+    return { user: null, token: null }
   }
 
 }
